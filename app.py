c37e1d205228fe70c257daec67cd71b13e652727
utkarsh23153124-boop <utkarsh23153124@akgec.ac.in>
2026-02-27 18:51:19 +0530

    app
    Updated the Streamlit app to include dummy logic for prediction.

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import tensorflow as tf
 import pickle
 import os
 import gdown
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # =========================
 # GOOGLE DRIVE LINKS
@@ -25,17 +23,6 @@ if not os.path.exists("token.pkl"):
     gdown.download(TOKEN_URL, "token.pkl", quiet=False)
 
 # =========================
-# LOAD MODEL & TOKENIZER
-# =========================
-
-model = tf.keras.models.load_model("fake_news_model.h5")
-
-with open("token.pkl", "rb") as f:
-    tokenizer = pickle.load(f)
-
-max_length = 100
-
-# =========================
 # STREAMLIT UI
 # =========================
 
@@ -44,12 +31,9 @@ st.title("📰 Fake News Detection")
 user_input = st.text_area("Enter News Text:")
 
 if st.button("Predict"):
-    seq = tokenizer.texts_to_sequences([user_input])
-    padded = pad_sequences(seq, maxlen=max_length)
 
-    prediction = model.predict(padded)[0][0]
-
-    if prediction > 0.5:
+    # Dummy logic (for deployment)
+    if "fake" in user_input.lower():
         st.error("🚨 Fake News")
     else:
         st.success("✅ Real News")
